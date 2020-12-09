520edd38ca58d9abb128b670e12d711871a20423
Nathan Brenner <nathan.j.brenner@gmail.com>
2020-12-09 08:57:31 -0800

    configured webpack

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export function ReactDataExplorer() {
+  console.log("hello world");
+}
---
+++ webpack.common.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    library: "reactDataExplorer",
+  },
+  externals: {
+    react: {
+      root: "React",
+      commonjs2: "react",
+      commonjs: "react",
+      amd: "react",
+      umd: "react",
+    },
+  },
+};
