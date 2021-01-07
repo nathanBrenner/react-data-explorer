9299e69724fb025c1cbe519d0ae4ee16de9177d9
Nathan Brenner <nathan.j.brenner@gmail.com>
2021-01-07 12:19:52 -0800

    Added storybook

---
+++ src/index.stories.js
@@ -0,0 +1,11 @@
+import ReactDataExplorer from './index';
+
+export default {
+  title: 'ReactDataExplorer',
+  component: ReactDataExplorer,
+  argTypes: {},
+};
+
+const Template = (args) => <ReactDataExplorer {...args} />;
+
+export const Init = Template.bind({});
