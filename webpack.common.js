b5e370ee15cb70c424cfcddd75530ba433ac1521
Nathan Brenner <nathan.j.brenner@gmail.com>
2021-01-10 15:12:57 -0800

    changed name of library

--- webpack.common.js
+++ webpack.common.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
-    library: 'reactDataExplorer',
+    library: 'ReactDataExplorer',
     libraryTarget: 'commonjs',
   },
   externals: {
