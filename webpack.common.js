72ddff5b7b71a061eb410c4b974e5ea8dd05176f
Nathan Brenner <nathan.j.brenner@gmail.com>
2021-01-10 14:30:26 -0800

    another publish attempt

--- webpack.common.js
+++ webpack.common.js
@@ -7,6 +7,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
     library: 'reactDataExplorer',
+    libraryTarget: 'commonjs',
   },
   externals: {
     react: {
